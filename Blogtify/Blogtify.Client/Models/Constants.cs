12fbf68560a70d0fcdb533ae094cd0258d825922
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-10-18 02:03:29 +0700

    update theme

--- Blogtify/Blogtify.Client/Models/Constants.cs
+++ Blogtify/Blogtify.Client/Models/Constants.cs
@@ -4,5 +4,10 @@ public static class Constants
 {
     public const int PageSize = 8;
     public const int SearchPageSize = 6;
+    public static List<string> Fonts = new List<string>() {
+        "Be Vietnam Pro", "Manrope",
+        "Inter", "Raleway",
+        "Source Code Pro", "Montserrat"
+    };
 
 }
