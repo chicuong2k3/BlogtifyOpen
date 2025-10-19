7f114ecccd2f91dfd179cd131aa5d6344122211d
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-10-19 16:57:15 +0700

    add mermaid + fix codeblock style

--- Blogtify/Blogtify.Client/Layout/BlogContentLayout.razor.js
+++ Blogtify/Blogtify.Client/Layout/BlogContentLayout.razor.js
@@ -95,3 +95,25 @@ export function renderMath(element) {
         MathJax.typesetPromise([element]).catch(err => console.error(err.message));
     }).catch(err => console.error('MathJax load error:', err));
 }
+
+export function Render(componentId, definition) {
+    const cb = function (svgGraph) {
+        //console.logg(svgGraph);
+    };
+    var elements = document.getElementsByClassName(componentId);
+    for (const element of elements) {
+        const diagramdefinition = htmlDecode(element.innerHTML);
+        const id = "mmd" + Math.round(Math.random() * 10000);
+        mermaid.render(`${id}-mermaid-svg`, diagramdefinition, (svg, bind) => {
+            const host = element;
+            host.innerHTML = svg
+            //bind(host);
+
+        });
+    }
+}
+
+function htmlDecode(input) {
+    var doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+}
